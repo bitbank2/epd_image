02cf6929bb5dd5f7445231d0795392a67d5c2a14
Laurence Bank <laurencebank@Laurences-Air.home>
2023-04-14 18:37:54 +0100

    Added Apache license info to code

--- main.c
+++ main.c
@@ -2,15 +2,24 @@
 // epd_image - prepare image data for e-paper displays
 // and output it as hex data ready to compile into c code
 //
-// Specifically - do pixel color matching for GRAY/BWR/BWY output
+// Specifically - do pixel color matching for GRAY/BW/BWR/BWY/BWYR output
 // from any input image, split it into the 1 or 2 memory planes
 // and prepare that data to be easily compiled into C code
 //
 // Written by Larry Bank
-// Copyright (c) 2023 BitBank Software, Inc.
-// Change history
-// 12/2/20 - Started the project
 //
+// Copyright 2023 BitBank Software, Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===========================================================================
+
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
